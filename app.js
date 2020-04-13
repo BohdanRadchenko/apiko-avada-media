2a297358836e1d261eeac5b24a51ced14aed83ba
BohdanRadchenko <radchenkobs@gmail.com>
2020-04-13 19:25:13 +0300

    test

--- app.js
+++ app.js
@@ -9,44 +9,43 @@ const PORT = process.env.PORT || config.get("port") || 5000
 const app = express()
 
 // express()
-    // .use(express.static(path.join(__dirname, 'client', 'build')))
-    // .get('/', (req, res) => res.render('index'))
-    // .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//     .use(express.static(path.join(__dirname, 'client', 'build')))
+//     .get('/', (req, res) => res.render('index'))
+//     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.use(express.static(path.join(__dirname, 'client', 'build')))
-app.get('/', (req, res) => res.render('index'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json({extended: true}));
 
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(express.json({extended: true}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+app.use('/api/users', require('./routes/user.routes'))
+app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/products', require('./routes/product.routers'))
+
+app.use('/',express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// const start = async () => {
+//     try {
+//         await mongoose.connect(config.get('mongoURL'), {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             useCreateIndex: true
+//         })
+//         app.listen(PORT, () => console.log(`app hes been started on port ${PORT} ...`))
+//     } catch (e) {
+//         console.log('Server Error', e.message)
+//         process.exit(1)
+//     }
+// }
 //
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     next();
-// });
-//
-// app.use('/api/users', require('./routes/user.routes'))
-// app.use('/api/auth', require('./routes/auth.routes'))
-// app.use('/api/products', require('./routes/product.routers'))
-//
-// app.use('/',express.static(path.join(__dirname, "client", "build")))
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
-// })
-
-const start = async () => {
-    try {
-        await mongoose.connect(config.get('mongoURL'), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
-        app.listen(PORT, () => console.log(`app hes been started on port ${PORT} ...`))
-    } catch (e) {
-        console.log('Server Error', e.message)
-        process.exit(1)
-    }
-}
-
-start()
+// start()
