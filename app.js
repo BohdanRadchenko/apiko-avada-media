a0db5f9bde5e9fc627cc45ff7c9129e52ee3ccf7
BohdanRadchenko <radchenkobs@gmail.com>
2020-04-13 20:17:21 +0300

    test

--- app.js
+++ app.js
@@ -22,37 +22,23 @@ app.use('/api/users', require('./routes/user.routes'))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/products', require('./routes/product.routers'))
 
-const mongoConnect = async () => {
-    await mongoose.connect(config.get('mongoURL'), {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    })
-}
-
 app.use('/', express.static(path.join(__dirname, "client", "build")))
 app.get("*", (req, res) => {
-    mongoConnect()
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
-// app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-
-
 const start = async () => {
-    // try {
-    //     await mongoose.connect(config.get('mongoURL'), {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //         useCreateIndex: true
-    //     })
-        // app.listen(PORT, () => console.log(`app hes been started on port ${PORT} ...`))
-    // } catch (e) {
-    //     console.log('Server Error', e.message)
-        // process.exit(1)
-    // }
+    try {
+        await mongoose.connect(config.get('mongoURL'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`app hes been started on port ${PORT} ...`))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
 }
 
-start()
-
- app.listen(PORT, () => console.log(`app hes been started on port ${PORT} ...`))
+start().then(console.log('wotk'))
