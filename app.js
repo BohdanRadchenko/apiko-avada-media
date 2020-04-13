f2ec75789531679738422467c46a768555e3b8f2
BohdanRadchenko <radchenkobs@gmail.com>
2020-04-13 18:57:40 +0300

    test

--- app.js
+++ app.js
@@ -8,24 +8,28 @@ const PORT = process.env.PORT || config.get("port") || 5000
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.json({extended: true}));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
-
-app.use('/api/users', require('./routes/user.routes'))
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/products', require('./routes/product.routers'))
-
-app.use('/',express.static(path.join(__dirname, "client", "build")))
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+app.get('/', () => {
+    console.log('work')
 })
+
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.use(express.json({extended: true}));
+//
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+// });
+//
+// app.use('/api/users', require('./routes/user.routes'))
+// app.use('/api/auth', require('./routes/auth.routes'))
+// app.use('/api/products', require('./routes/product.routers'))
+//
+// app.use('/',express.static(path.join(__dirname, "client", "build")))
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+// })
 
 const start = async () => {
     try {
